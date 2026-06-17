4cda963b8383fc5aa87dd1ed0f03464b9c4f8013
Momi <sonzaisinai@sinai.com>
2026-06-17 14:19:58 +0900

    update:DBanMemo
    メモの場所を変えたのと、要件定義書を少し変更したのと、DBの制約を少しいじりました。

--- DB/テーブル作成/追加制約.sql
+++ DB/テーブル作成/追加制約.sql
@@ -7,3 +7,21 @@ ALTER TABLE choices
 ADD CONSTRAINT unique_choice_per_question
 UNIQUE (question_id, choice_text);
 
+
+//questionsにexternal_keyを追加
+ALTER TABLE questions
+ADD COLUMN IF NOT EXISTS external_key varchar(50);
+
+//ユニーク制約を追加
+CREATE UNIQUE INDEX IF NOT EXISTS uq_questions_external_key
+ON questions (external_key);
+
+
+//choicesにもユニーク制約を追加
+CREATE UNIQUE INDEX IF NOT EXISTS uq_choices_question_order
+ON choices (question_id, choice_order);
+
+
+//現在時刻を保持するカラムを追加
+ALTER TABLE answer_histories
+ADD COLUMN answered_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP;
